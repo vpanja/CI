56656ac98cb21e1e05b09a4ee1cf917e034a92c3
Venugopal Panja <venugopal.panja@realpage.com>
2018-08-01 17:03:23 +0530

    database change

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8012/CI/';
+$config['base_url'] = $_SERVER['REQUEST_SCHEME'] .'://'.$_SERVER['HTTP_HOST'].'/CI/';
 //$config['base_url'] ='';
 
 /*
